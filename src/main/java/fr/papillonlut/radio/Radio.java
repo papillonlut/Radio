7dbd7c04659baff9a9082bcd664cf851ed6eddae
anton <antonirenault21@gmail.com>
2024-02-09 16:26:47 +0100

    test

---
+++ src/main/java/fr/papillonlut/radio/Radio.java
@@ -0,0 +1,16 @@
+package fr.papillonlut.radio;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Radio implements ModInitializer {
+	public static final String MOD_ID = "radio";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/fr/papillonlut/radio/RadioClient.java
@@ -0,0 +1,10 @@
+package fr.papillonlut.radio;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RadioClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
